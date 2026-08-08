54769cb9e42e50eed892c169b1d0fe633abe73e1
I202316631 <I202316631@cibertec.edu.pe>
2026-08-08 00:38:26 -0500

    Ignorar log.txt

---
+++ Flixlatam/src/main/kotlin/com/byayzen/TmdbConfig.kt
@@ -0,0 +1,5 @@
+package com.byayzen
+
+// Este archivo NO se sube a git (agregado a .gitignore).
+// Mantiene la API key de TMDB fuera del control de versiones.
+internal const val TMDB_API_KEY = "6b5396ac1a07b4173afa4ea90b4564ec"

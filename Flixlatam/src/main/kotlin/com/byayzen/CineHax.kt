f498769d850210ebe92294de2f71cce5e9c8ff61
I202316631 <I202316631@cibertec.edu.pe>
2026-09-21 19:39:56 -0500

    Trigger build with corrected workflow

--- Flixlatam/src/main/kotlin/com/byayzen/CineHax.kt
+++ Flixlatam/src/main/kotlin/com/byayzen/CineHax.kt
@@ -20,7 +20,7 @@ import android.util.Log
 
 class CineHax : MainAPI() {
     override var mainUrl = "https://cinehax.com"
-    override var name = "CineHax" // Trigger build 10
+    override var name = "CineHax"
     override val hasMainPage = true
     override var lang = "es"
     override val hasQuickSearch = false
